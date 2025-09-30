cca7a789c0c11785bf29003d1ebd09551f3d2fb8
kthygreat <156484866+kthygreat@users.noreply.github.com>
2025-09-29 21:32:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MY-WEB-DESIGN-PRO/',   // 👈 add this line
+  base: '/MY-WEB-DESIGN-PRO/',   
 })
 
 
